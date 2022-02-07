8a7048fe12a1174abd49c45dee26fe846411c22e
Lahcene Benmouloud <lahcenebenmouloud@gmail.com>
2022-02-07 15:24:39 +0100

    Front

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -1,6 +1,174 @@
 @extends('layouts.front')
 
 @section('content')
+
+<!-- Header Section Start -->
+<div class="header-section">
+
+    <!-- Header Top Start -->
+    <div class="header-top d-none d-lg-block">
+        <div class="container">
+
+            <!-- Header Top Wrapper Start -->
+            <div class="header-top-wrapper">
+
+                <!-- Header Top Left Start -->
+                <div class="header-top-left">
+                    <p>Never Stop <a href="#" style="color: #aa896b">Learning.</a></p>
+                </div>
+                <!-- Header Top Left End -->
+
+                <!-- Header Top Medal Start -->
+                <div class="header-top-medal">
+                    <div class="top-info">
+                        <p><i style="color: #aa896b" class="flaticon-phone-call"></i> (213) 0553 007 364</p>
+                        <p><i style="color: #aa896b" class="flaticon-email"></i> contact@dahliasinstitute.com</p>
+                    </div>
+                </div>
+                <!-- Header Top Medal End -->
+
+                <!-- Header Top Right Start -->
+                <div class="header-top-right">
+                    <ul class="social">
+                        <li><a href="https://www.facebook.com/dahliasinstitute"><i class="flaticon-facebook"></i></a></li>
+                        <li><a href="#"><i class="flaticon-instagram"></i></a></li>
+                    </ul>
+                </div>
+                <!-- Header Top Right End -->
+
+            </div>
+            <!-- Header Top Wrapper End -->
+
+        </div>
+    </div>
+    <!-- Header Top End -->
+
+    <!-- Header Main Start -->
+    <div class="header-main">
+        <div class="container">
+
+            <!-- Header Main Start -->
+            <div class="header-main-wrapper">
+
+                <!-- Header Logo Start -->
+                <div class="header-logo">
+                    <a href="{{asset('/')}}"><img src="{{asset('front/assets/images/logo.png')}}" alt="Logo"></a>
+                </div>
+                <!-- Header Logo End -->
+
+                <!-- Header Menu Start -->
+                <div class="header-menu d-none d-lg-block">
+                    <ul class="nav-menu">
+                        <li><a href="{{asset('/')}}">Accueil</a></li>
+                        <li>
+                            <a href="#">ٌRubriques</a>
+                            <ul class="sub-menu">
+                                @foreach ($categories as $categorie)
+                                <li><a href="{{asset('category-courses/'.$categorie->id)}}">{{$categorie->name}}</a></li>
+                                @endforeach
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">A propos</a>
+                        </li>
+                        <li><a href="{{asset('/contact')}}">Contact</a></li>
+                    </ul>
+
+                </div>
+                <!-- Header Menu End -->
+
+                <!-- Header Sing In & Up Start -->
+                <div class="header-sign-in-up d-none d-lg-block">
+                    <ul>
+                        <li><a class="sign-up" href="{{asset('/login')}}">Connexion</a></li>
+                    </ul>
+                </div>
+                <!-- Header Sing In & Up End -->
+
+                <!-- Header Mobile Toggle Start -->
+                <div class="header-toggle d-lg-none">
+                    <a class="menu-toggle" href="javascript:void(0)">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                </div>
+                <!-- Header Mobile Toggle End -->
+
+            </div>
+            <!-- Header Main End -->
+
+        </div>
+    </div>
+    <!-- Header Main End -->
+
+</div>
+<!-- Header Section End -->
+
+<!-- Mobile Menu Start -->
+<div class="mobile-menu">
+
+    <!-- Menu Close Start -->
+    <a class="menu-close" href="javascript:void(0)">
+        <i class="icofont-close-line"></i>
+    </a>
+    <!-- Menu Close End -->
+
+    <!-- Mobile Top Medal Start -->
+    <div class="mobile-top">
+        <p><i class="flaticon-phone-call"></i> (213) 0553 007 364</p>
+        <p><i class="flaticon-email"></i>contact@dahliasinstitute.com</p>
+    </div>
+    <!-- Mobile Top Medal End -->
+
+    <!-- Mobile Sing In & Up Start -->
+    <div class="mobile-sign-in-up">
+        <ul>
+            <li><a class="sign-Up" href="{{asset('/login')}}">Connexion</a></li>
+        </ul>
+    </div>
+    <!-- Mobile Sing In & Up End -->
+
+    <!-- Mobile Menu Start -->
+    <div class="mobile-menu-items">
+        <ul class="nav-menu">
+            <li><a href="{{asset('/')}}">Accueil</a></li>
+            <li>
+                <a href="#">Rubriques</a>
+                <ul class="sub-menu">
+                    @foreach ($categories as $categorie)
+                    <li><a href="{{asset('category-courses/'.$categorie->id)}}">{{$categorie->name}}</a></li>
+                    @endforeach
+                  
+                </ul>
+            </li>
+            <li>
+                <a href="#">A propos </a>
+              
+            </li>
+            
+            <li><a href="{{asset('/contact')}}">Contact</a></li>
+        </ul>
+
+    </div>
+    <!-- Mobile Menu End -->
+
+    <!-- Mobile Menu End -->
+    <div class="mobile-social">
+        <ul class="social">
+            <li><a href="#"><i class="flaticon-facebook"></i></a></li>
+            <li><a href="#"><i class="flaticon-instagram"></i></a></li>
+        </ul>
+    </div>
+    <!-- Mobile Menu End -->
+
+</div>
+<!-- Mobile Menu End -->
+
+<!-- Overlay Start -->
+<div class="overlay"></div>
+<!-- Overlay End -->
+
   <!-- Slider Start -->
 <div class="section slider-section">
 
